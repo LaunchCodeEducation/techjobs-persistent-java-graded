8a993d125d0db141ae0ef01b9dad44ed919d91f0
Rich Ives <rjsives89@gmail.com>
2022-10-30 20:12:13 -0500

    GA#4 Part 1 complete - connected to MYSQL DB techjobs  AbstractEntity Updated per Part 2 instructions

--- queries.sql
+++ queries.sql
@@ -1,5 +1,9 @@
 -- Part 1: Test it with SQL
-
+/* SHOW COLUMNS FROM job
+Field: id TYPE: int
+Field: employer TYPE: VARCHAR
+Field: name TYPE: VARCHAR
+Field: skills TYPE: VARCHAR */
 -- Part 2: Test it with SQL
 
 -- Part 3: Test it with SQL
